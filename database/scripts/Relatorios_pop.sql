043fb788b661d707f6669b5031e519e3e3f030c0
Isa Alegrette <isa.alegrette@gmail.com>
2023-10-01 01:04:48 -0300

    organizing db folder

---
+++ database/scripts/Relatorios_pop.sql
@@ -0,0 +1,17 @@
+INSERT INTO relatorios (rel_sigla, rel_ano, rel_titulo) 
+VALUES ('SOA01', '2006', 'The State of Agile Development'), 
+('SOA02', '2007', '2nd Annual Survey: “The State of Agile Development”'),
+('SOA03', '2008', '3rd Annual Survey: 2008 - “The State of Agile Development”'),
+('SOA04', '2009', '4th Annual Survey: “The State of Agile Development”'),
+('SOA05', '2010', '5th Annual Survey: “The State of Agile Development”'),
+('SOA06', '2011', '6th Annual Survey: “The State of Agile Development”'),
+('SOA07', '2012', '7th Annual State of Agile Development Survey'),
+('SOA08', '2013', '8th Annual State of Agile Survey'),
+('SOA09', '2014', '9th Annual State of Agile Survey'),
+('SOA10', '2015', 'The 10th Annual State of Agile Report'),
+('SOA11', '2016', 'The 11th Annual State of Agile Report'),
+('SOA12', '2017', 'The 12th Annual State of Agile Report'),
+('SOA13', '2018', 'The 13th Annual State of Agile Report'),
+('SOA14', '2019', 'The 14th Annual State of Agile Report'),
+('SOA15', '2021', 'The 15th Annual State of Agile Report'),
+('SOA16', '2022', 'The 16th Annual State of Agile Report');
