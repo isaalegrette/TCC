9853fb26147317cd6308e0dc971adeed94095990
Isa Alegrette <isa.alegrette@gmail.com>
2023-09-30 23:10:46 -0300

    Initial commit

---
+++ backend/metodologias-ageis/src/main/java/com/tcc/metodologiasageis/MetodologiasAgeisApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.metodologiasageis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodologiasAgeisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodologiasAgeisApplication.class, args);
+	}
+
+}
