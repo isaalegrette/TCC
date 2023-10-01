4787c06c0654fde07da1c87a1b72bfe7139dd4eb
Isa Alegrette <isa.alegrette@gmail.com>
2023-10-01 01:00:02 -0300

    organizing db folder

--- banco de dados/select respostas com metrica e relatorio.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM tcc.respostas;
-
-SELECT rel_sigla, met_sigla, sub_descricao, res_valor, res_id FROM Relatorios 
-INNER JOIN Respostas ON  rel_id = fk_Relatorios_rel_id 
-INNER JOIN Submetricas ON fk_Submetricas_sub_id = sub_id
-INNER JOIN Metricas ON fk_Metricas_met_id = met_id
-ORDER BY res_id;
